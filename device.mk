6b4d53e538fd2bc6a57c1efd80c492ff2a5928ba
Pierre-Hugues Husson <phh@phh.me>
2021-08-09 15:10:09 +0530

    spaced: Add back MTK IMS Overlays
    Signed-off-by: DrtSinX98 <pritish@stag-os.org>

--- device.mk
+++ device.mk
@@ -84,6 +84,11 @@ PRODUCT_PACKAGES += \
 DEVICE_PACKAGE_OVERLAYS += \
     $(DEVICE_PATH)/overlay
 
+# MTK IMS Overlays
+PRODUCT_PACKAGES += \
+    mtk-ims \
+    mtk-ims-telephony
+
 # Permissions
 PRODUCT_COPY_FILES += \
     $(DEVICE_PATH)/permissions/privapp-permissions-mediatek.xml:$(TARGET_COPY_OUT_SYSTEM)/etc/permissions/privapp-permissions-mediatek.xml
