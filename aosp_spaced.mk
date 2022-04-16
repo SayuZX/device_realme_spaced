8cf48888d09d7b07a08f678b99bd9ac2e7de09ce
jhenrique09 <jhsv09@gmail.com>
2022-04-16 08:19:31 +0200

    spaced: Enable quick tap support
    Signed-off-by: DrtSinX98 <pritish@stag-os.org>

--- aosp_spaced.mk
+++ aosp_spaced.mk
@@ -23,6 +23,7 @@ $(call inherit-product, device/realme/spaced/device.mk)
 
 # Inherit some common AOSP stuff.
 $(call inherit-product, vendor/aosp/config/common_full_phone.mk)
+TARGET_SUPPORTS_QUICK_TAP := true
 TARGET_BOOT_ANIMATION_RES := 1080
 
 # Device identifier. This must come after all inclusions.
